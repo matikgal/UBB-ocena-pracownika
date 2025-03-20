d5d3df3215a844d517862cfd0edf5eaae3247500
GaloszJakub <kubagalosz@gmail.com>
2025-03-20 13:57:05 +0100

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import React from "react";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/AppSideBar";
 
 function App() {
-	return (
-		<>
-			<div>
-				<Button variant="ghost">siema</Button>
-			</div>
-		</>
-	)
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+      </main>
+    </SidebarProvider>
+  );
 }
 
-export default App
+export default App;
