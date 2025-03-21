b54339dfb9a89504bbb703c1ff11003d3787baee
Mateusz <77492310+matikgal@users.noreply.github.com>
2025-03-21 18:40:16 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { SidebarProvider} from './components/ui/sidebar'
+import { SidebarProvider } from './components/ui/sidebar'
 import { AppSidebar } from './components/AppSideBar'
 import AppHeader from './components/AppHeader'
-import AppHeader from "./components/AppHeader";
-
 
 function App() {
 	return (
@@ -10,7 +8,6 @@ function App() {
 			<AppSidebar />
 			<main className="w-screen">
 				<AppHeader />
-				<SidebarTrigger />
 			</main>
 		</SidebarProvider>
 	)
--- src/components/AppSideBar.tsx
+++ src/components/AppSideBar.tsx
@@ -1,75 +1,58 @@
-import { BookOpen, Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { BookOpen, Home } from 'lucide-react'
 
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarTrigger,
-} from "../components/ui/sidebar"
-
-
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuItem,
+} from '../components/ui/sidebar'
 
 // Menu items.
 const items = [
-  {
-    title: "Ocena",
-    url: "#",
-    icon: Home,
-  },
+	{
+		title: 'Ocena',
+		url: '#',
+		icon: Home,
+	},
 ]
 
 const categories = [
-  {title : "Publikacje dykaktyczne",
-    url: "#",
-    icon: BookOpen
-  },
-  {title : "Podniesienie jakości nauczania",
-    url: "#",
-    icon: BookOpen
-  } ,
-  {title : "Zajęcia w języku obcym, wykłady za granicą",
-    url: "#",
-    icon: BookOpen
-  } ,
-  {title : "Pełnienie funkcji dydaktycznej (za każdy rok)",
-    url: "#",
-    icon: BookOpen
-  } ,
-  {title : "Nagrody i wyróznienia",
-    url: "#",
-    icon: BookOpen
-  } 
+	{ title: 'Publikacje dykaktyczne', url: '#', icon: BookOpen },
+	{ title: 'Podniesienie jakości nauczania', url: '#', icon: BookOpen },
+	{ title: 'Zajęcia w języku obcym, wykłady za granicą', url: '#', icon: BookOpen },
+	{ title: 'Pełnienie funkcji dydaktycznej (za każdy rok)', url: '#', icon: BookOpen },
+	{ title: 'Nagrody i wyróznienia', url: '#', icon: BookOpen },
 ]
 
-
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      
-      <SidebarContent>
-        <SidebarGroup>
-          
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-          
-            <SidebarMenu>
-              <div><h2 className="font-bold text-2xl text-white mt-16">Tu sie oceniasz</h2></div>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title} className="flex flex-col">
-                          {categories.map((category) => (
-                            <div key={category.title} className="text-white text-wrap max-w-[16rem] m-2 hover:underline cursor-pointer font-semibold ">{category.title}</div>
-                          ))}               
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  )
+	return (
+		<Sidebar>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							<div>
+								<h2 className="font-bold text-2xl text-white mt-16">Tu sie oceniasz</h2>
+							</div>
+							{items.map(item => (
+								<SidebarMenuItem key={item.title} className="flex flex-col">
+									{categories.map(category => (
+										<div
+											key={category.title}
+											className="text-white text-wrap max-w-[16rem] m-2 hover:underline cursor-pointer font-semibold ">
+											{category.title}
+										</div>
+									))}
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	)
 }
