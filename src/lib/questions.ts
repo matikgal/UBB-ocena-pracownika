196a315b1c8bd6a28560db011306368bab47d635
GaloszJakub <kubagalosz@gmail.com>
2025-03-23 14:50:10 +0100

    punkty

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,52 +1,51 @@
 export const publikacjeDydaktyczne = [
-    { id: "1.1", title: "Autorstwo podręcznika akademickiego w języku obcym" },
-    { id: "1.2", title: "Autorstwo podręcznika akademickiego w języku polskim" },
-    { id: "1.3", title: "Autorstwo skryptu" },
-    { id: "1.4", title: "Autorstwo artykułu/monografii (dotyczy pracowników dydaktycznych)" },
-    { id: "1.5", title: "Tłumaczenie podręcznika akademickiego na język polski" },
-    { id: "1.6", title: "Tłumaczenie skryptu na język polski" },
-    { id: "1.7", title: "Autorstwo wspólnych publikacji ze studentami (artykuł)" },
-    { id: "1.8", title: "Autorstwo rozdziału w podręczniku akademickim" },
-    { id: "1.9", title: "Autorstwo publikacji metodycznej lub popularnonaukowej" },
-    { id: "1.10", title: "Recenzja podręcznika akademickiego lub szkolnego" },
-    { id: "1.11", title: "Redakcja wydawnictwa zwartego zawierającego recenzowane publikacje studentów" }
-  ];
-  
-  export const podniesienieJakosciNauczania = [
-    { id: "2.1", title: "Koordynator/Kierownik międzynarodowego projektu dydaktycznego" },
-    { id: "2.2", title: "Koordynator/Kierownik wniosku o finansowanie międzynarodowego projektu dydaktycznego" },
-    { id: "2.3", title: "Koordynator/Kierownik krajowego projektu dydaktycznego" },
-    { id: "2.4", title: "Koordynator/Kierownik wniosku o finansowanie krajowego projektu dydaktycznego" },
-    { id: "2.5", title: "Opracowanie i wdrożenie nowej specjalności studiów" },
-    { id: "2.6", title: "Autorstwo i realizacja programu nowego przedmiotu" },
-    { id: "2.7", title: "Nowe stanowisko do zajęć laboratoryjnych lub program komputerowy" },
-    { id: "2.8", title: "Przygotowanie lub modyfikacja programu studiów podyplomowych" },
-    { id: "2.9", title: "Przygotowanie programów nowych kursów specjalistycznych lub dokształcających" },
-    { id: "2.10", title: "Jakość prowadzenia zajęć dydaktycznych (arkusze hospitacji)" },
-    { id: "2.11", title: "Jakość prowadzenia zajęć dydaktycznych (ankiety studenckie i doktoranckie)" },
-    { id: "2.12", title: "Promotorstwo prac dyplomowych licencjackich lub inżynierskich" },
-    { id: "2.13", title: "Promotorstwo prac dyplomowych magisterskich" },
-    { id: "2.14", title: "Przygotowanie recenzji prac licencjackich, inżynierskich lub magisterskich" },
-    { id: "2.15", title: "Prowadzenie dodatkowych zajęć sportowych lub sekcji sportowej" },
-    { id: "2.16", title: "Przygotowanie przedmiotu w systemie e-learningowym" },
-    { id: "2.17", title: "Organizacja i przeprowadzenie centralnego egzaminu językowego" }
-  ];
-  
-  export  const zajeciaJezykObcy = [
-    { id: "3.1", title: "Wykłady dla studentów w ośrodku zagranicznym (np. Erasmus+)" },
-    { id: "3.2", title: "Prowadzenie zajęć w języku obcym oraz zajęć dla studentów zagranicznych" },
-    { id: "3.3", title: "Konsultacje ze studentami zagranicznymi w języku obcym" }
-  ];
-  
-  export  const funkcjeDydaktyczne = [
-    { id: "4.1", title: "Członkostwo w Uczelnianej komisji ds. jakości kształcenia" },
-    { id: "4.2", title: "Członkostwo w Wydziałowej komisji ds. jakości kształcenia" },
-    { id: "4.3", title: "Opiekun praktyk studenckich lub koordynator praktyk na kierunku" },
-    { id: "4.4", title: "Kierowanie edycją studiów podyplomowych" }
-  ];
-  
-  export  const nagrodyWyroznienia = [
-    { id: "5.1", title: "Indywidualna lub zespołowa nagroda Ministra Edukacji i Nauki za osiągnięcia dydaktyczne" },
-    { id: "5.2", title: "Promotorstwo nagrodzonych prac dyplomowych lub doktorskich" }
-  ];
-  
+  { id: "1.1", title: "Autorstwo podręcznika akademickiego w języku obcym", points: 40 },
+  { id: "1.2", title: "Autorstwo podręcznika akademickiego w języku polskim", points: 30 },
+  { id: "1.3", title: "Autorstwo skryptu", points: 20 },
+  { id: "1.4", title: "Autorstwo artykułu/monografii (dotyczy pracowników dydaktycznych)", points: "Punktacja wg systemu bibliotecznego" },
+  { id: "1.5", title: "Tłumaczenie podręcznika akademickiego na język polski", points: 15 },
+  { id: "1.6", title: "Tłumaczenie skryptu na język polski", points: 10 },
+  { id: "1.7", title: "Autorstwo wspólnych publikacji ze studentami (artykuł)", points: 5 },
+  { id: "1.8", title: "Autorstwo rozdziału w podręczniku akademickim", points: 5 },
+  { id: "1.9", title: "Autorstwo publikacji metodycznej lub popularnonaukowej", points: 3 },
+  { id: "1.10", title: "Recenzja podręcznika akademickiego lub szkolnego", points: 5 },
+  { id: "1.11", title: "Redakcja wydawnictwa zwartego zawierającego recenzowane publikacje studentów", points: 10 }
+];
+
+export const podniesienieJakosciNauczania = [
+  { id: "2.1", title: "Koordynator/Kierownik międzynarodowego projektu dydaktycznego", points: 10 },
+  { id: "2.2", title: "Koordynator/Kierownik wniosku o finansowanie międzynarodowego projektu dydaktycznego", points: 10 },
+  { id: "2.3", title: "Koordynator/Kierownik krajowego projektu dydaktycznego", points: 8 },
+  { id: "2.4", title: "Koordynator/Kierownik wniosku o finansowanie krajowego projektu dydaktycznego", points: 8 },
+  { id: "2.5", title: "Opracowanie i wdrożenie nowej specjalności studiów", points: 5 },
+  { id: "2.6", title: "Autorstwo i realizacja programu nowego przedmiotu", points: 2 },
+  { id: "2.7", title: "Nowe stanowisko do zajęć laboratoryjnych lub program komputerowy", points: 5 },
+  { id: "2.8", title: "Przygotowanie lub modyfikacja programu studiów podyplomowych", points: 8 },
+  { id: "2.9", title: "Przygotowanie programów nowych kursów specjalistycznych lub dokształcających", points: 5 },
+  { id: "2.10", title: "Jakość prowadzenia zajęć dydaktycznych (arkusze hospitacji)", points: "Zależne od ocen" },
+  { id: "2.11", title: "Jakość prowadzenia zajęć dydaktycznych (ankiety studenckie)", points: "Zależne od ocen" },
+  { id: "2.12", title: "Promotorstwo prac dyplomowych licencjackich lub inżynierskich", points: 2 },
+  { id: "2.13", title: "Promotorstwo prac dyplomowych magisterskich", points: 3 },
+  { id: "2.14", title: "Przygotowanie recenzji prac licencjackich, inżynierskich lub magisterskich", points: 0.5 },
+  { id: "2.15", title: "Prowadzenie dodatkowych zajęć sportowych lub sekcji sportowej", points: 5 },
+  { id: "2.16", title: "Przygotowanie przedmiotu w systemie e-learningowym", points: 3 },
+  { id: "2.17", title: "Organizacja i przeprowadzenie centralnego egzaminu językowego", points: 3 }
+];
+
+export const zajeciaJezykObcy = [
+  { id: "3.1", title: "Wykłady dla studentów w ośrodku zagranicznym", points: 0.5 },
+  { id: "3.2", title: "Prowadzenie zajęć w języku obcym oraz zajęć dla studentów zagranicznych", points: 0.5 },
+  { id: "3.3", title: "Konsultacje ze studentami zagranicznymi w języku obcym", points: 0.5 }
+];
+
+export const funkcjeDydaktyczne = [
+  { id: "4.1", title: "Członkostwo w Uczelnianej komisji ds. jakości kształcenia", points: 5 },
+  { id: "4.2", title: "Członkostwo w Wydziałowej komisji ds. jakości kształcenia", points: 5 },
+  { id: "4.3", title: "Opiekun praktyk studenckich", points: 3 },
+  { id: "4.4", title: "Kierowanie edycją studiów podyplomowych", points: 3 }
+];
+
+export const nagrodyWyroznienia = [
+  { id: "5.1", title: "Indywidualna lub zespołowa nagroda Ministra Edukacji i Nauki", points: 20 },
+  { id: "5.2", title: "Promotorstwo nagrodzonych prac dyplomowych", points: 2 }
+];
