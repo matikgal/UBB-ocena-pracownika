e07a482904dc0b1cc3b9ba2d7caca1d7993dbb1d
Mateusz <77492310+matikgal@users.noreply.github.com>
2025-03-23 15:32:01 +0100

    questioncomponent

--- src/components/QuestionsComponent.tsx
+++ src/components/QuestionsComponent.tsx
@@ -33,30 +33,30 @@ export default function QuestionsComponent({ selectedCategory }: QuestionsCompon
 	const questions = categoryMap[selectedCategory] || publikacjeDydaktyczne
 
 	return (
-		<div className="p-6 bg-white rounded-b-3xl shadow-md mx-auto min-h-full  overflow-hidden">
+		<div className="p-6 bg-white rounded-b-3xl shadow-md mx-auto h-[calc(100vh-160px)] flex flex-col overflow-hidden">
 			<h2 className="text-2xl font-semibold text-gray-800 mb-6 border-b-2 border-blue-500 pb-2">{selectedCategory}</h2>
-			<ul className="list-decimal pl-6 space-y-3">
-				{questions.map(question => (
-					<li
-						key={question.id}
-						className="text-gray-700 hover:text-blue-600 transition-colors duration-200 flex items-end text-2xl">
-						<span className="font-bold text-blue-500">
-							{question.id}.({question.points})
-						</span>{' '}
-						{question.title}
-						<TooltipProvider>
-							<Tooltip>
-								<TooltipTrigger>
-									<GoInfo className="scale-75" />
-								</TooltipTrigger>
-								<TooltipContent>
-									<p>{question.tooltip} asd</p>
-								</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					</li>
-				))}
-			</ul>
+			<div className="overflow-y-auto flex-grow">
+				<ul className="list-decimal pl-6 space-y-3">
+					{questions.map(question => (
+						<li key={question.id} className="text-gray-700 flex flex-col text-xl">
+							<div className="flex items-center">
+								<span className="font-bold text-blue-500 mr-2">{question.id}</span> {question.title}
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger>
+											<GoInfo className="scale-75 ml-2" />
+										</TooltipTrigger>
+										<TooltipContent>
+											<p>{question.tooltip}</p>
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+							</div>
+							<p className="font-thin text-lg">Maksymalna ilość punktów ({question.points})</p>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
