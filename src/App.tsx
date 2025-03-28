4d2cb56b6d25802e73ec573a4699803555314ed7
Mateusz <77492310+matikgal@users.noreply.github.com>
2025-03-28 20:14:33 +0100

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import AppHeader from './components/AppHeader'
 import QuestionsComponent from './components/QuestionsComponent'
 import { useState } from 'react'
@@ -24,21 +23,6 @@ function App() {
 				</div>
 			</main>
 		</div>
-=======
-import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar'
-import { AppSidebar } from './components/AppSideBar'
-import AppHeader from './components/AppHeader'
-
-function App() {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<main className="w-screen">
-				<AppHeader />
-				<SidebarTrigger />
-			</main>
-		</SidebarProvider>
->>>>>>> Stashed changes
 	)
 }
 
